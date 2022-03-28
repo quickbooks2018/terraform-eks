f710e3b4c48f71ef653666c938c5fb865b442ec3
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-03-28 05:05:49 +0000

    eks workers role named added

--- terraform/environment/dev/main.tf
+++ terraform/environment/dev/main.tf
@@ -89,6 +89,7 @@ module "eks" {
   cluster_name              = "cloudgeeks-eks-dev"
   vpc_id                    = module.eks_vpc.vpc_id
   subnets                   = module.eks_vpc.private_subnets
+  workers_role_name         = "iam-eks-workers-role"
   create_eks                = true
   manage_aws_auth           = false
   write_kubeconfig          = false
